8c6a19aa3b326ba3189d2c9faad9396d0ce55d12
kutiku <kutiku@ad5613cf-68cf-4bbd-83cb-646bdbc1459d>
2009-03-06 13:43:15 +0000

    Unittest für AgencyQuery

--- test/AgencyQueryTest.php
+++ test/AgencyQueryTest.php
@@ -1,16 +1,14 @@
-#!/usr/bin/php
 <?php
-error_reporting(E_ALL);
 
+require_once 'PHPUnit/Framework.php';
 require_once dirname(__FILE__)."/../classes/autoloader/BAV_Autoloader.php";
-BAV_Autoloader::add('../classes/BAV.php');
 BAV_Autoloader::add('../classes/dataBackend/BAV_DataBackend_PDO.php');
 
 
 /**
  * check BAV_DataBackend_PDO->getAgencies($sql)
  *
- * Copyright (C) 2007  Markus Malkusch <bav@malkusch.de>
+ * Copyright (C) 2009  Markus Malkusch <bav@malkusch.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,14 +24,13 @@ BAV_Autoloader::add('../classes/dataBackend/BAV_DataBackend_PDO.php');
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * @package scripts
- * @subpackage test
+ * @package test
  * @author Markus Malkusch <bav@malkusch.de>
- * @copyright Copyright (C) 2007 Markus Malkusch
+ * @copyright Copyright (C) 2009 Markus Malkusch
  */
 
 
-class BAV_CheckAgencyQuery extends BAV {
+class AgencyQueryTest extends PHPUnit_Framework_TestCase {
 
 
     /**
@@ -44,92 +41,75 @@ class BAV_CheckAgencyQuery extends BAV {
 
     /**
      */
-    public function __construct() {
+    protected function setUp() {
         $this->backend = new BAV_DataBackend_PDO(new PDO('mysql:host=localhost;dbname=test', 'test'));
-        $this->testNoID();
-        
-        $this->backend = new BAV_DataBackend_PDO(new PDO('mysql:host=localhost;dbname=test', 'test'));
-        $this->testNoBank();
-        
-        $this->backend = new BAV_DataBackend_PDO(new PDO('mysql:host=localhost;dbname=test', 'test'));
-        $this->testOnlyID();
-        
-        $this->backend = new BAV_DataBackend_PDO(new PDO('mysql:host=localhost;dbname=test', 'test'));
-        $this->testIDAndBank();
     }
     
     
     public function testOnlyID() {
         $agencies = $this->backend->getAgencies(
             'SELECT id FROM bav_agency LIMIT 100');
-        $this->verifyAgencies($agencies, 100);
+        $this->assertAgencies($agencies, 100);
     }
     
     
     public function testIDAndBank() {
         $agencies = $this->backend->getAgencies(
             'SELECT id, bank FROM bav_agency LIMIT 100');
-        $this->verifyAgencies($agencies, 100);
+        $this->assertAgencies($agencies, 100);
     }
     
     
     public function testNoBank() {
         $agencies = $this->backend->getAgencies(
             'SELECT id, name, postcode, city, shortTerm, pan, bic FROM bav_agency LIMIT 100');
-        $this->verifyAgencies($agencies, 100);
+        $this->assertAgencies($agencies, 100);
     }
     
     
+    /**
+     * @expectedException BAV_DataBackendException_IO_MissingAttributes
+     */
     public function testNoID() {
-        try {
-            $result = $this->backend->getAgencies(
-                'SELECT name, postcode, city, shortTerm, pan, bic, bank FROM bav_agency LIMIT 1');
-            throw new LogicException();
-
-        } catch (BAV_DataBackendException_IO_MissingAttributes $e) {
-            // OK
-        }
+        $result = $this->backend->getAgencies(
+            'SELECT name, postcode, city, shortTerm, pan, bic, bank FROM bav_agency LIMIT 1'
+        );
     }
     
     
-    private function verifyAgencies(Array $agencies, $count) {
-        if (count($agencies) !== $count) {
-            throw new LogicException("Too less agencies (".count($agencies)." vs. $count).");
-
-        }
+    private function assertAgencies(Array $agencies, $count) {
+    	$this->assertEquals($count, count($agencies));
+    	
         foreach ($agencies as $agency) {
             if ($agency->isMainAgency()) {
-                if ($agency->getBank()->getMainAgency() !== $agency) {
-                    throw new LogicException("Inconsistent Main Agency");
-
-                }
-                if ($agency->getBank() !== $agency->getBank()->getMainAgency()->getBank()) {
-                    throw new LogicException("Inconsistent Bank");
-
-                }
+            	$this->assertTrue(
+                    $agency->getBank()->getMainAgency() === $agency,
+                    "Inconsistent Main Agency"
+                );
+                $this->assertTrue(
+                    $agency->getBank() === $agency->getBank()->getMainAgency()->getBank(),
+                    "Inconsistent Bank"
+                );
+                
             } else {
                 $includedCount = 0;
                 foreach ($agency->getBank()->getAgencies() as $banksAgency) {
-                    if ($banksAgency->getBank() !== $agency->getBank()) {
-                        throw new LogicException("Inconsistent bank");
-
-                    }
+                	$this->assertTrue(
+                	    $banksAgency->getBank() === $agency->getBank(),
+                	    "Inconsistent bank"
+                	);
                     if ($banksAgency === $agency) {
                         $includedCount++;
 
                     }
                 }
-                if ($includedCount !== 1) {
-                    throw new LogicException("Inconsistent agency");
-
-                }
+                $this->assertEquals(1, $includedCount, "Inconsistent agency");
             }
         }
     }
 
 
 }
-new BAV_CheckAgencyQuery();
 
 
 ?>
