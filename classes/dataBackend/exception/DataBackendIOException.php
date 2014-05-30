765630ed24dea953bca8f06ffb1930c4c03fd596
Markus Malkusch <markus@malkusch.de>
2014-05-31 00:43:27 +0200

    fixed missing namespace

--- classes/dataBackend/exception/DataBackendIOException.php
+++ classes/dataBackend/exception/DataBackendIOException.php
@@ -31,7 +31,7 @@ class DataBackendIOException extends DataBackendException
     /**
      * Constructs the exception
      */
-    public function __construct($message = "", $code = 0, Exception $previous = null)
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
--- classes/exception/BAVException.php
+++ classes/exception/BAVException.php
@@ -14,7 +14,7 @@ class BAVException extends \Exception
     /**
      * Constructs the exception
      */
-    public function __construct($message = "", $code = 0, Exception $previous = null)
+    public function __construct($message = "", $code = 0, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
