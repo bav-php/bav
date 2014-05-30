6395181a5cc5eb02d3451c8cfc710837f5b89949
Markus Malkusch <markus@malkusch.de>
2014-05-30 22:22:05 +0200

    travis PDO environment

---
+++ tests/bin/test-install.php
@@ -0,0 +1,30 @@
+#!/bin/env php
+<?php
+/**
+ * Installs the test environment
+ *
+ * @author Markus Malkusch <markus@malkusch.de>
+ * @license GPL
+ */
+
+namespace malkusch\bav;
+
+require_once __DIR__ . "/../../autoloader/autoloader.php";
+
+try {
+    ConfigurationRegistry::getConfiguration()->setAutomaticInstallation(false);
+    $databack = ConfigurationRegistry::getConfiguration()->getDatabackendContainer()->getDataBackend();
+    
+    // install file
+    $databack->install();
+    echo "Bundesbank file downloaded.\n";
+
+    // install PDO
+    $pdoContainer = new PDODataBackendContainer(PDOFactory::makePDO());
+    $pdoContainer->getDataBackend()->install();
+    echo "PDO installed.\n";
+
+} catch (DataBackendException $error) {
+    die("Installation failed: {$error->getMessage()}\n");
+
+}
