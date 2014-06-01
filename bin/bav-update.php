d35ffca47fc94a46a4524c55b0c01298a0ee0630
Markus Malkusch <markus@malkusch.de>
2014-06-01 22:18:01 +0200

    disable update plan in bav-update.php

--- bin/bav-update.php
+++ bin/bav-update.php
@@ -13,6 +13,7 @@ namespace malkusch\bav;
 require_once __DIR__ . "/../autoloader/autoloader.php";
 
 try {
+    ConfigurationRegistry::getConfiguration()->setUpdatePlan(null);
     $databack = ConfigurationRegistry::getConfiguration()->getDatabackendContainer()->getDataBackend();
 
     $databack->update();
