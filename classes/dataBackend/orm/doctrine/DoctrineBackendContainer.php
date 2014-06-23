393fccbfa293da85a40aded638423fc54bf17136
Markus Malkusch <markus@malkusch.de>
2014-06-24 01:37:04 +0200

    implemented installation and update

---
+++ classes/dataBackend/orm/doctrine/DoctrineBackendContainer.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace malkusch\bav;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+
+/**
+ * Container for DoctrineDataBackend objects.
+ * 
+ * You will need Doctrine as composer dependency.
+ * 
+ * @author Markus Malkusch <markus@malkusch.de>
+ * @license GPL
+ * @see DataBackend
+ * @link http://www.doctrine-project.org/
+ */
+class DoctrineBackendContainer extends DataBackendContainer
+{
+    
+    /**
+     * @var EntityManager 
+     */
+    private $em;
+    
+    /**
+     * Return the paths to the XML-Mappings
+     * 
+     * @return string[]
+     */
+    public static function getXMLMappings()
+    {
+        return array(__DIR__ . "/mapping/");
+    }
+    
+    /**
+     * Builds a container for a connection.
+     * 
+     * @param array $connection Doctrine::DBAL connection
+     * @return DoctrineBackendContainer
+     */
+    public static function buildByConnection(array $connection, $isDevMode = false)
+    {
+        $mappings = self::getXMLMappings();
+        $config = Setup::createXMLMetadataConfiguration($mappings, $isDevMode);
+
+        $entityManager = EntityManager::create($connection, $config);
+        return new self($entityManager);
+    }
+    
+    /**
+     * Injects the EntityManager 
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+    
+    /**
+     * Gets the EntityManager
+     * 
+     * @return EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->em;
+    }
+    
+    protected function makeDataBackend()
+    {
+        return new DoctrineDataBackend($this->em);
+    }
+}
---
+++ classes/dataBackend/orm/doctrine/DoctrineDataBackend.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace malkusch\bav;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+
+/**
+ * Use Doctrine ORM as backend.
+ *
+ * You will need Doctrine as composer dependency.
+ * 
+ * @author Markus Malkusch <markus@malkusch.de>
+ * @license GPL
+ * @link http://www.doctrine-project.org/
+ */
+class DoctrineDataBackend extends DataBackend
+{
+
+    /**
+     * @var EntityManager 
+     */
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    protected function getNewBank($bankID)
+    {
+        
+    }
+
+    public function getAgenciesForBank(Bank $bank)
+    {
+        
+    }
+
+    public function getAllBanks()
+    {
+        
+    }
+
+    public function getBICAgencies($bic)
+    {
+        
+    }
+
+    public function getLastUpdate()
+    {
+        
+    }
+
+    public function getMainAgency(Bank $bank)
+    {
+        
+    }
+    
+    private function getClassesMetadata()
+    {
+        return array(
+            $this->em->getClassMetadata('malkusch\bav\Bank'),
+            $this->em->getClassMetadata('malkusch\bav\Agency'),
+        );
+    }
+
+    public function install()
+    {
+        $tool = new SchemaTool($this->em);
+        $classes = $this->getClassesMetadata();
+        $tool->createSchema($classes);
+        
+        $this->update();
+    }
+
+    public function isInstalled()
+    {
+        
+    }
+
+    public function uninstall()
+    {
+        $tool = new SchemaTool($this->em);
+        $classes = $this->getClassesMetadata();
+        $tool->dropSchema($classes);
+    }
+
+    public function update()
+    {
+        $this->em->transactional(function ($em) {
+            
+            // Download data
+            $fileUtil = new FileUtil();
+            $fileBackend = new FileDataBackend(tempnam($fileUtil->getTempDirectory(), 'bav'));
+            $fileBackend->install();
+
+            // Delete all
+            $em->createQuery("DELETE FROM malkusch\bav\Agency")->execute();
+            $em->createQuery("DELETE FROM malkusch\bav\Bank")->execute();
+
+            // Inserting data
+            foreach ($fileBackend->getAllBanks() as $bank) {
+                try {
+                    $em->persist($bank);
+                    
+                    $agencies = $bank->getAgencies();
+                    $agencies[] = $bank->getMainAgency();
+                    foreach ($agencies as $agency) {
+                        $em->persist($agency);
+                        
+                    }
+                } catch (NoMainAgencyException $e) {
+                    trigger_error(
+                        "Skipping bank {$e->getBank()->getBankID()} without any main agency."
+                    );
+                }
+            }
+        });
+    }
+}
