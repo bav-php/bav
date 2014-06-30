f6ef7756f17b2b4092ad2dbf69b970d13254a7ad
Markus Malkusch <markus@malkusch.de>
2014-06-30 13:33:39 +0200

    test configuration initialization of ConfigurationRegistry

---
+++ tests/unit/ConfigurationRegistryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace malkusch\bav;
+
+require_once __DIR__ . "/../bootstrap.php";
+
+/**
+ * Tests ConfigurationRegistryTest
+ *
+ * @author Markus Malkusch <markus@malkusch.de>
+ * @licends GPL
+ * @see ConfigurationRegistry
+ */
+class ConfigurationRegistryTest extends \PHPUnit_Framework_TestCase
+{
+    
+    /**
+     * Tests the initialization with a configuration from the include path.
+     * 
+     * @see ConfigurationRegistry::classConstructor()
+     */
+    public function testInitWithIncludePathConfiguration()
+    {
+        set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . "/../data/");
+        
+        ConfigurationRegistry::classConstructor();
+        $configuration = ConfigurationRegistry::getConfiguration();
+        
+        $this->assertInstanceOf("malkusch\bav\Configuration", $configuration);
+        $this->assertEquals("test", $configuration->getTempDirectory());
+        
+        restore_include_path();
+    }
+    
+    /**
+     * Tests the initialization with the default configuration
+     * 
+     * @see ConfigurationRegistry::classConstructor()
+     */
+    public function testInitWithDefaultConfiguration()
+    {
+        ConfigurationRegistry::classConstructor();
+        $configuration = ConfigurationRegistry::getConfiguration();
+        
+        $this->assertInstanceOf("malkusch\bav\DefaultConfiguration", $configuration);
+        $this->assertNotEquals("test", $configuration->getTempDirectory());
+    }
+}
