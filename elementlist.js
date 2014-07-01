dacdede38ceac4ade87b2dc0397fb554da97fd8a
Markus Malkusch <markus@malkusch.de>
2014-07-01 10:30:25 +0200

    added the backend container to the api

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","malkusch\\bav\\Agency"],["c","malkusch\\bav\\AutomaticUpdatePlan"],["c","malkusch\\bav\\BAV"],["c","malkusch\\bav\\Configuration"],["c","malkusch\\bav\\ConfigurationRegistry"],["c","malkusch\\bav\\DefaultConfiguration"],["c","malkusch\\bav\\LogUpdatePlan"],["c","malkusch\\bav\\UpdatePlan"]];
+ApiGen.elements = [["c","malkusch\\bav\\Agency"],["c","malkusch\\bav\\AutomaticUpdatePlan"],["c","malkusch\\bav\\BAV"],["c","malkusch\\bav\\Configuration"],["c","malkusch\\bav\\ConfigurationRegistry"],["c","malkusch\\bav\\DataBackendContainer"],["c","malkusch\\bav\\DefaultConfiguration"],["c","malkusch\\bav\\DoctrineBackendContainer"],["c","malkusch\\bav\\FileDataBackendContainer"],["c","malkusch\\bav\\LogUpdatePlan"],["c","malkusch\\bav\\PDODataBackendContainer"],["c","malkusch\\bav\\UpdatePlan"],["c","PDO"]];
