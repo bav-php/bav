fb26ad2bf8a5c5dfc27bf147952fda9d7a6306eb
Markus Malkusch <markus@malkusch.de>
2014-07-01 09:51:09 +0200

    new api

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","malkusch\\bav\\Agency"],["c","malkusch\\bav\\AutomaticUpdatePlan"],["c","malkusch\\bav\\BAV"],["c","malkusch\\bav\\Configuration"],["c","malkusch\\bav\\ConfigurationRegistry"],["c","malkusch\\bav\\DefaultConfiguration"],["c","malkusch\\bav\\LogUpdatePlan"],["c","malkusch\\bav\\UpdatePlan"]];
