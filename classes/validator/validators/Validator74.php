d555a94127729d81d5689419b94856c5997efc22
Markus Malkusch <markus@malkusch.de>
2015-01-13 01:45:30 +0100

    [PR18] Fix Validator74 for e.g. 15 - 5 = 10.

--- classes/validator/validators/Validator74.php
+++ classes/validator/validators/Validator74.php
@@ -43,7 +43,7 @@ class Validator74 extends Validator00
 
         } elseif (strlen(ltrim($this->account, '0')) == 6) {
             $nextHalfDecade = round($this->accumulator/10) * 10 + 5;
-            $check          = $nextHalfDecade - $this->accumulator;
+            $check          = ($nextHalfDecade - $this->accumulator) % 10;
             return (string) $check === $this->getChecknumber();
 
         } else {
